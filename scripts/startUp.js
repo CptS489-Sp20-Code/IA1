ea3be9b4fb0ef1d0cb046d2b2efbcd0942563f9a
Chris Hundhausen <chundhau@gmail.com>
2020-01-22 11:25:11 -0800

    Updated baseed on Sp20 feedback

--- scripts/startUp.js
+++ scripts/startUp.js
@@ -1,6 +1,5 @@
 //Start-up functions run when page is loaded.
-includeHTML();
-document.getElementById("profileDiv").style.display = "none";
-document.getElementById("roundsDiv").style.display = "none";
-document.getElementById("coursesDiv").style.display = "none";
-document.getElementById("logRoundDiv").style.display = "none";
+includeHTML(); //Include html
+//TO DO: Write additional code to be executed at startup, e.g.,
+document.getElementById("page1").style.display = "none";
+
